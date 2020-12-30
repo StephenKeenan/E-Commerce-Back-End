381cb3297a1db83667e3bee91e91e99096466a80
Stephen Keenan <stephenkeenan02@gmail.com>
2020-12-29 20:44:53 -0500

    Formatting and tag code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
@@ -13,7 +13,7 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    
+
     product_id: {
       type: DataTypes.INTEGER,
       references: {
