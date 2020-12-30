6c4ec6ca16387be6679b7c287f0038bdb14f38bd
Stephen Keenan <stephenkeenan02@gmail.com>
2020-12-29 19:25:16 -0500

    schema.sql code

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,32 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT,
+    category_name TYPE:STRING NOT NULL, 
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE product (
+    id INTO NOT NULL AUTO_INCREMENT,
+    product_name TYPE:STRING NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT REFERENCES ID,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id)
+)
+
+CREATE TABLE product_tag (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT REFERENCES ID,
+    tag_id INT REFERENCES ID,
+    PRIMARY KEY (id)
+)
+
+    CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT,
+    tag_name STR,
+    PRIMARY KEY (id)
+)
