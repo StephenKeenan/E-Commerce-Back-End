e6e981150208beb3b6a8c40e74fb20864509e9e1
Stephen Keenan <stephenkeenan02@gmail.com>
2021-01-04 17:27:05 -0500

    Tutor work

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../../config/connection.js');
+const sequelize = require('../config/connection.js');
 
 class Category extends Model { }
 
@@ -8,7 +8,7 @@ Category.init(
   {
     // define columns
     id: {
-      type: DataTypes.Integer,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const routes = require('./routes');
+// const routes = require('./routes');
 require('./config/connection');
 
 var consoleTable = require('console-table');
